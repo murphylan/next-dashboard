d608d42b8179aaab614c73c28d7b04af63c5642d
lanzejun <lanzejun@cn.ibm.com>
2023-12-02 22:04:20 +0800

    update

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,15 @@
-import {
-  fetchCardData,
-  fetchLatestInvoices,
-} from '@/app/lib/data';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard'
+};
 
 export default async function Page() {
   return (
