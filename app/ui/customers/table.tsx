e1cc82639493bccc7f944a05d6250016775dc11c
lanzejun <lanzejun@cn.ibm.com>
2023-12-03 19:35:20 +0800

    update

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '../search';
-import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
+import { CustomersTable as CustomersTableType, FormattedCustomersTable } from '@/app/lib/definitions';
 
 export default async function CustomersTable({
   customers,
@@ -10,10 +10,10 @@ export default async function CustomersTable({
 }) {
   return (
     <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+      {/* <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
         Customers
       </h1>
-      <Search placeholder="Search customers..." />
+      <Search placeholder="Search customers..." /> */}
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
